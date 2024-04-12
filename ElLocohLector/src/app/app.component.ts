d43f07a7c630624f2a676d7b67ea1815c1a4a091
Miguel Leiva <129530762+aeternus020298@users.noreply.github.com>
2024-04-12 17:40:22 -0400

    Component Header
    Se agrega componente header con logo, texto y button, es responsivo con media query

--- ElLocohLector/src/app/app.component.ts
+++ ElLocohLector/src/app/app.component.ts
@@ -1,45 +1,20 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { BodyComponent } from "./components/body/body.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,HttpClientModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HttpClientModule, HeaderComponent, BodyComponent, FooterComponent]
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'ElLocohLector';
 
-  data: any = [];
-  apiUrl = 'http://localhost:8080/libros'; // URL de tu API
-
-  constructor(private http: HttpClient) { }
-
-  ngOnInit(): void {
-    
-    this.llamarApi().then(() => {
-      for (const libro of this.data) {
-        console.log(libro.titulo);
-      }
-    });
-
-  }
-  llamarApi(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.http.get<any>(this.apiUrl).subscribe(
-        response => {
-          this.data = response;
-          resolve();
-        },
-        error => {
-          console.error('Error al obtener datos:', error);
-          reject(error);
-        }
-      );
-    });
-  }
 }
 
 
---
+++ ElLocohLector/src/app/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatCardModule,MatFormFieldModule,MatInputModule,MatButtonModule,MatIconModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
