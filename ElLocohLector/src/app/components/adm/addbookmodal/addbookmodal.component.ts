63fabe6e497110a7623acebc57cc8787da799df6
Miguel Leiva <129530762+aeternus020298@users.noreply.github.com>
2024-06-16 18:08:01 -0400

    actualizacion addbook

--- ElLocohLector/src/app/components/adm/addbookmodal/addbookmodal.component.ts
+++ ElLocohLector/src/app/components/adm/addbookmodal/addbookmodal.component.ts
@@ -9,11 +9,12 @@ import { ConnectService } from '../../../modules/lobby/services/connect.service'
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
-
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIcon } from '@angular/material/icon';
 @Component({
   selector: 'app-addbookmodal',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatAutocompleteModule,CommonModule ],
+  imports: [MatFormFieldModule, MatTooltipModule, MatIcon, MatInputModule, FormsModule, ReactiveFormsModule, MatButtonModule, MatAutocompleteModule,CommonModule ],
   templateUrl: './addbookmodal.component.html',
   styleUrl: './addbookmodal.component.css'
 })
@@ -44,6 +45,12 @@ export class AddbookmodalComponent implements OnInit{
   nombreCategorias: string[] = [];
   listaEdiciones: any = [];
   nombreEdiciones: string[] = [];
+
+  nuevoAutor = false;
+  nuevaEditorial = false;
+  nuevaCategoria = false;
+  nuevaEdicion = false;
+tooltipMessage: any;
 
   constructor(
     public dialogRef: MatDialogRef<AddbookmodalComponent>,
@@ -96,9 +103,10 @@ export class AddbookmodalComponent implements OnInit{
   }
 
   onSubmit(): void {
+    //llamo a la funcion buscarAutores()
     const selectedAutor = this.listaAutores.find((autor: any) => {
-      const nombreCompleto = autor.apellido_autor 
-        ? `${autor.nombre_autor} ${autor.apellido_autor}` 
+      const nombreCompleto = autor.apellido_autor
+        ? `${autor.nombre_autor} ${autor.apellido_autor}`
         : autor.nombre_autor;
       return nombreCompleto === this.autorControl.value;
     });
@@ -114,18 +122,52 @@ export class AddbookmodalComponent implements OnInit{
     this.nuevoLibro.categoria = selectedCategoria ? selectedCategoria.id_categoria : null;
     this.nuevoLibro.edicion = selectedEdicion ? selectedEdicion.id_edicion : null;
 
-    console.log('Enviando libro:', this.nuevoLibro);
-    this.connectService.agregarLibro(this.nuevoLibro).subscribe(
-      response => {
-        console.log('Libro agregado:', response);
-        this.dialogRef.close(response);
-      },
-      error => {
-        console.error('Error al agregar libro:', error);
-      }
-    );
+    if (!selectedAutor && this.nuevoAutor) {
+      const nombreAutor = this.autorControl.value;
+      this.connectService.agregarAutor({ nombre_autor: nombreAutor }).subscribe(response => {
+        this.listaAutores.push(response); // Agrega el nuevo autor a la lista
+        this.getNombresAutores(); // Actualiza la lista de nombres de autores
+        this.nuevoLibro.autor = response.id_autor; // Actualiza el ID del libro con el nuevo ID de autor
+        console.log('Nuevo autor agregado:', response); // Log para verificar el nuevo autor agregado
+        this.submitBook();
+      });
+    } else if (!selectedEditorial && this.nuevaEditorial) {
+      const nombreEditorial = this.editorialControl.value;
+      this.connectService.agregarEditorial({ nombre_editorial: nombreEditorial }).subscribe(response => {
+        this.listaEditoriales.push(response); // Agrega la nueva editorial a la lista
+        this.getNombresEditoriales(); // Actualiza la lista de nombres de editoriales
+        this.nuevoLibro.editorial = response.id_editorial; // Actualiza el ID del libro con el nuevo ID de editorial
+        console.log('Nueva editorial agregada:', response); // Log para verificar la nueva editorial agregada
+        this.submitBook();
+      });
+    } else if (!selectedCategoria && this.nuevaCategoria) {
+      const nombreCategoria = this.categoriaControl.value;
+      this.connectService.agregarCategoria({ nombre_categoria: nombreCategoria }).subscribe(response => {
+        this.listaCategorias.push(response); // Agrega la nueva categoría a la lista
+        this.getNombresCategorias(); // Actualiza la lista de nombres de categorías
+        this.nuevoLibro.categoria = response.id_categoria; // Actualiza el ID del libro con el nuevo ID de categoría
+        console.log('Nueva categoría agregada:', response); // Log para verificar la nueva categoría agregada
+        this.submitBook();
+      });
+    } else if (!selectedEdicion && this.nuevaEdicion) {
+      const nombreEdicion = this.edicionControl.value;
+      this.connectService.agregarEdicion({ edicion: nombreEdicion }).subscribe(response => {
+        this.listaEdiciones.push(response); // Agrega la nueva edición a la lista
+        this.getNombresEdiciones(); // Actualiza la lista de nombres de ediciones
+        this.nuevoLibro.edicion = response.id_edicion; // Actualiza el ID del libro con el nuevo ID de edición
+        console.log('Nueva edición agregada:', response); // Log para verificar la nueva edición agregada
+        this.submitBook();
+      });
+    } else {
+      this.submitBook();
+    }
   }
-
+  submitBook(): void {
+    this.connectService.agregarLibro(this.nuevoLibro).subscribe(response => {
+      console.log('Libro añadido:', response);
+      this.dialogRef.close();
+    });
+  }
   closeDialog(): void {
     this.dialogRef.close();
   }
@@ -151,4 +193,47 @@ export class AddbookmodalComponent implements OnInit{
   getNombresEdiciones(): void {
     this.nombreEdiciones = this.listaEdiciones.map((edicion: any) => edicion.edicion);
   }
+
+//Esta funcion toma solamente el nombre de la lista de autores y lo itera
+  buscarAutores(nombreBuscado: string, apellidoBuscado: string): boolean {
+    // Iterar sobre la lista de autores
+    for (let autor of this.listaAutores) {
+      // Verificar si el nombre y apellido coinciden
+      if (autor.nombre_autor === nombreBuscado && autor.apellido_autor === apellidoBuscado) {
+        return true; // Retornar true si se encuentra una coincidencia exacta
+      }
+    }
+    return false; // Retornar false si no se encuentra ninguna coincidencia
+  }
+  
+  //Esta funcion toma solamente el nombre de la lista de categoria y lo itera
+  buscarCategoria(categoriaBuscada: string): boolean {
+    for (let categoria of this.listaCategorias) {
+      if (categoria.nombre_categoria === categoriaBuscada) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  //Esta funcion toma solamente el nombre de la lista de editorial y lo itera
+  buscarEditorial(editorialBuscada: string): boolean {
+    for (let editorial of this.listaEditoriales) {
+      if (editorial.nombre_editorial === editorialBuscada) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  //Esta funcion toma solamente el nombre de la lista de edicion y lo itera
+  buscarEdicion(edicionBuscada: string): boolean {
+    for (let edicion of this.listaEdiciones) {
+      if (edicion.edicion === edicionBuscada) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
 }
