845ebbefe871581bdb33e5990be1b73955035658
Miguel Leiva <129530762+aeternus020298@users.noreply.github.com>
2024-06-16 18:34:04 -0400

    Carcasa de modal ejemplares creado
     falta traer los datos de la tabla ejemplares

---
+++ ElLocohLector/src/app/components/adm/ejemplares/ejemplares.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplares',
+  standalone: true,
+  imports: [],
+  templateUrl: './ejemplares.component.html',
+  styleUrl: './ejemplares.component.css'
+})
+export class EjemplaresComponent {
+
+}
