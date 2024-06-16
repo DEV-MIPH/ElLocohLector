9ff4312916a16521a04d64259075eeba751cc266
Miguel Leiva <129530762+aeternus020298@users.noreply.github.com>
2024-06-16 19:54:39 -0400

    Se desarrolla page de pedidos
    se desarrolla pestaña para recibir los pedidos del usuario, falta que se reciban correctamente cuando el usuario este logeado y apriete para solicitar el libro y se cargue aqui

--- ElLocohLector/src/app/pages/pedidos/pedidos.component.ts
+++ ElLocohLector/src/app/pages/pedidos/pedidos.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
+interface LibroPedido {
+  titulo: string;
+  autor: string;
+  editorial: string;
+}
 
 @Component({
     selector: 'app-pedidos',
     standalone: true,
     templateUrl: './pedidos.component.html',
     styleUrl: './pedidos.component.css',
-    imports: [HeaderComponent, FooterComponent]
+    imports: [MatTableModule, MatPaginatorModule, MatSortModule, HeaderComponent, FooterComponent]
 })
-export class PedidosComponent {
+export class PedidosComponent implements OnInit{
+  displayedColumns: string[] = ['titulo', 'autor', 'editorial'];
+  dataSource = new MatTableDataSource<LibroPedido>([]);
+  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  agregarLibro(libro: LibroPedido) {
+    this.dataSource.data = [...this.dataSource.data, libro];
+  }
 }
