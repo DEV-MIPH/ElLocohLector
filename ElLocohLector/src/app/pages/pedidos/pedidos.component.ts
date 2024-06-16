8b76a9b27980acc067a56471620193cf61274959
Miguel Leiva <129530762+aeternus020298@users.noreply.github.com>
2024-06-16 19:10:43 -0400

    Se agrega carcasa de pedidos

---
+++ ElLocohLector/src/app/pages/pedidos/pedidos.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+import { FooterComponent } from "../../components/footer/footer.component";
+
+@Component({
+    selector: 'app-pedidos',
+    standalone: true,
+    templateUrl: './pedidos.component.html',
+    styleUrl: './pedidos.component.css',
+    imports: [HeaderComponent, FooterComponent]
+})
+export class PedidosComponent {
+
+}
