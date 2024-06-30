c6f343ef2d0403535707b199fb85f1c73628b0e2
Miguel Leiva <129530762+aeternus020298@users.noreply.github.com>
2024-06-30 02:05:06 -0400

    Actualizacion de Contacto y responsive
    Se utiliza form angular, se mejora lo visual y se deja responsive

--- ElLocohLector/src/app/components/contacto/contacto.component.ts
+++ ElLocohLector/src/app/components/contacto/contacto.component.ts
@@ -1,26 +1,67 @@
 import { Component } from '@angular/core';
+
+//Importaciones
+import { FormControl, FormGroupDirective, NgForm, Validators, FormsModule, ReactiveFormsModule, AbstractControl, ValidationErrors, ValidatorFn  } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ErrorStateMatcher } from '@angular/material/core';
+
 import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
+
+//importación a conectService
+import { ConnectService } from '../../modules/lobby/services/connect.service';
+
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [MatSnackBarModule],
+  imports: [MatSnackBarModule,FormsModule, MatInputModule, ReactiveFormsModule, MatFormFieldModule],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
 
-  constructor(private snackBar: MatSnackBar){}
+  constructor(private connectService: ConnectService, private snackBar: MatSnackBar){}
+
+  institucion: string = '';
+  email: string = '';
+  comentario: string = '';
+
+  //Validación para institucion
+  institucionValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const valid = control.value && control.value.trim() !== '';
+      return valid ? null : { institucion: true };
+    };
+  }
+  //Validación para comentario
+  comentarioValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const valid = control.value && control.value.trim() !== '';
+      return valid ? null : { comentario: true };
+    };
+  }
+
+
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  institucionFormControl = new FormControl('', [Validators.required, this.institucionValidator()]);
+  comentarioFormControl = new FormControl('', [Validators.required, this.comentarioValidator()]);
+  matcher = new MyErrorStateMatcher();
 
   enviarFormulario(event: Event) {
     event.preventDefault(); // Evita el envío del formulario
 
-    // Aquí puedes agregar la lógica para enviar el formulario usando HTTP, etc.
-
-    // Mostrar el Snackbar de éxito
-    this.snackBar.open('Formulario enviado con éxito!', 'Cerrar', {
-      duration: 3000, // Duración en milisegundos
-    });
+  
+   
 
     // Restablecer el formulario
     const form = event.target as HTMLFormElement;
